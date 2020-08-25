5f4398fd667eb8c3e53702664c1ebaf643e5d981
carloslima27 <carloslimaplays2017@gmail.com>
2020-08-25 16:15:19 -0300

    Classe Vendedor

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,13 +1,17 @@
 package aplicacao;
 
+import java.util.Date;
+
 import modelo.entidades.Departamento;
+import modelo.entidades.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
 		Departamento d = new Departamento(1, "Eletronicos");
-		System.out.println(d);
+		Vendedor v = new Vendedor(2, "Alex", "Alex@gmail.com", new Date(), 3000.00, d);
+		System.out.println(v);
 
 	}
 
