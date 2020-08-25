1c5cae7905e5f3f6153633e7a85a77a5fad6fabf
carloslima27 <carloslimaplays2017@gmail.com>
2020-08-25 15:57:44 -0300

    Classe Departamento

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,14 @@
+package aplicacao;
+
+import modelo.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamento d = new Departamento(1, "Eletronicos");
+		System.out.println(d);
+
+	}
+
+}
